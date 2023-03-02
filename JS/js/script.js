990b868a9226e3c48a8aba03a7e2a0f53dfdab3a
Gitros <31899790+Gitros@users.noreply.github.com>
2023-03-02 19:32:01 +0100

    typy danych

---
+++ JS/js/script.js
@@ -0,0 +1,23 @@
+
+// Typy danych
+
+// typy proste
+
+//string
+const name = 'lily';
+
+// number
+
+const age = 1234.5;
+
+//bolean 
+
+const isTrue = true;
+
+//null & undefinded 
+
+const test = null;
+let test2
+
+console.log(test);
+console.log(test2);
