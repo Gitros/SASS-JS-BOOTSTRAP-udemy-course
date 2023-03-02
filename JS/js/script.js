095c0a4c87675b9685ef537134dbfa48e9444f76
Gitros <31899790+Gitros@users.noreply.github.com>
2023-03-02 19:53:47 +0100

    if

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,1 +1,10 @@
 
+const passLength = 7;
+
+if (passLength >= 10) {
+    console.log("dobre hasło");
+} else if (passLength > 5 && passLength < 10) {
+    console.log("spk hasło");
+} else {
+    console.log('słabe hasło.');
+}
