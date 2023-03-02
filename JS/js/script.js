f7f026f969f5d897f26e75c181ea37925bfdd99a
Gitros <31899790+Gitros@users.noreply.github.com>
2023-03-02 19:54:11 +0100

    clear

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,10 +1,2 @@
 
-const passLength = 7;
-
-if (passLength >= 10) {
-    console.log("dobre hasło");
-} else if (passLength > 5 && passLength < 10) {
-    console.log("spk hasło");
-} else {
-    console.log('słabe hasło.');
-}
+const colors = []
