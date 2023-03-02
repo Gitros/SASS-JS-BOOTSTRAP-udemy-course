c3362946e82af0d9922d6cb46bb8b80d97b4431d
Gitros <31899790+Gitros@users.noreply.github.com>
2023-03-02 20:04:12 +0100

    function

---
+++ JS/js/script.js
@@ -0,0 +1,16 @@
+function test() {
+    console.log("function!");
+}
+
+test()
+
+function addNumbers(x, y) {
+    console.log(x + y);
+}
+
+addNumbers(5, 10);
+
+function showName(name) {
+    console.log(`Podane imie to ${name}`);
+}
+showName('klaudia')
