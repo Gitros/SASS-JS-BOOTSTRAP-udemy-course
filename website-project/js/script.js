9e879644650bfcd976c62feca0db9e0baa576e96
Gitros <31899790+Gitros@users.noreply.github.com>
2023-03-09 21:54:01 +0100

    section about pt.2

--- website-project/js/script.js
+++ website-project/js/script.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-
     const nav = document.querySelector('.navbar');
+    const allNavItems = document.querySelectorAll('.nav-link');
+    const navList = document.querySelector('.navbar-collapse');
 
     function addShadow() {
         if (window.scrollY >= 100) {
@@ -10,6 +11,10 @@ document.addEventListener('DOMContentLoaded', function () {
             nav.classList.remove('shadow-bg')
         }
     }
+
+    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+
+
 
     window.addEventListener('scroll', addShadow)
 
